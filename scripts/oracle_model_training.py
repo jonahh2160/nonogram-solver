913215e86cecea6ea3ce714a87c56559b7b24580
John Regensburger <jregensburger@murraystate.edu>
2025-12-06 19:35:36 -0600

    JR - changed accuracy metric of compiler

--- scripts/oracle_model_training.py
+++ scripts/oracle_model_training.py
@@ -3,40 +3,29 @@ from oracle_model import OracleModel as model
 import numpy as np
 import tensorflow as tf
 
-x_train = np.load(config.X_TRAIN)['arr_0']
-y_train = np.load(config.Y_TRAIN)['arr_0']
-x_test = np.load(config.X_TEST)['arr_0']
-y_test = np.load(config.Y_TEST)['arr_0']
-
-x_train = tf.convert_to_tensor(x_train, dtype=tf.float32)
-y_train = tf.convert_to_tensor(y_train, dtype=tf.float32)
-x_test = tf.convert_to_tensor(x_test, dtype=tf.float32)
-y_test = tf.convert_to_tensor(y_test, dtype=tf.float32)
+x_train = np.load(config.X_TRAIN)['arr_0'].astype(np.float32)
+y_train = np.load(config.Y_TRAIN)['arr_0'].astype(np.float32)
+x_test = np.load(config.X_TEST)['arr_0'].astype(np.float32)
+y_test = np.load(config.Y_TEST)['arr_0'].astype(np.float32)
 
 model.compile(
     optimizer = 'adam',
     loss = 'binary_crossentropy', # Binary classification
-    metrics = ['accuracy']
+    metrics = ['binary_accuracy'] # Checks the average accuracy of predictions by cell
 )
 
-X = x_train
-Y = y_train
-
 model.fit(
-    X,
-    Y,
-    epochs = 32,
-    batch_size = 64,
+    x_train,
+    y_train,
+    epochs = config.EPOCHS,
+    batch_size = config.BATCH_SIZE,
     validation_split = 0.1,
     shuffle = True    
 )
 
-X = x_test
-Y = y_test
-
 model.evaluate(
-    X,
-    Y
+    x_test,
+    y_test
 )
 
 prediction = model.predict(x_train[:1])[0]
